4566455ab8c674a9c7e13c3f1e39f5ad48fa3f3b
MariiaChernetska <mariachernetska@outlook.com>
2017-06-07 17:56:55 +0300

    first commit

---
+++ AuthService/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AuthService.Controllers
+{
+    [RoutePrefix("auth")]
+    public class AuthController : ApiController
+    {
+        [Route("{Device_ID}")]
+        public IHttpActionResult Get(string Device_ID) {
+            if (!String.IsNullOrEmpty(Device_ID)) {
+                return Ok();
+            }
+            return InternalServerError();
+        }
+    }
+}
